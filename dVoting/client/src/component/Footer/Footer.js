e78c89c503b6d659bad4452fdf66acad7ba980da
Tukaram-Bhawar <tukarambhawar014496@gmail.com>
2024-11-27 21:51:08 +0530

    initial commit

---
+++ dVoting/client/src/component/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          TSSM Final Year Project Group 2
+          
+        </p>
+        <p>
+          Made with <i className="fas fa-heartbeat" /> by{" "} TAPSR
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
